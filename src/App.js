58cf47773eeefe4662af8a60413a14b5de03c5ca
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-06-19 01:49:25 +0600

    dark mode implement

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
       <Flex w="100%">
         <Heading
           ml="8" size="md" fontWeight='semibold' color="cyan.400">imthepk</Heading>
+
+        <Spacer></Spacer>
+        <IconButton icon={<FaLinkedin />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaInstagram />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaGithub />} isRound='true' onClick={toggleColorMode}></IconButton>
         <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
       </Flex>
       </VStack>
